bbf2653f673e61b5729991ef3daf66b628d9dbff
KatherinAllin28 <68928423+KatherinAllin28@users.noreply.github.com>
2025-04-15 00:32:35 -0500

    Update state_manager.py

--- server/state_manager.py
+++ server/state_manager.py
@@ -9,29 +9,20 @@ class StateManager:
         self.state = self._load_state()
 
     def _load_state(self):
-        """Carga el estado de los tópicos desde el archivo JSON."""
         if not os.path.exists(self.state_file):
-            return {}  # Estado vacío si no existe el archivo
+            return {}
         with open(self.state_file, "r") as f:
             return json.load(f)
 
     def save_state(self):
-        """Guarda el estado actual en el archivo JSON."""
         with open(self.state_file, "w") as f:
             json.dump(self.state, f, indent=4)
 
-    def get_topics(self):
-        return self.state
-
-    def add_topic(self, topic_name, partitions):
-        self.state[topic_name] = {"partitions": partitions}
+    def add_topic(self, topic_name, num_partitions):
+        self.state[topic_name] = {"partitions": num_partitions}
         self.save_state()
 
     def delete_topic(self, topic_name):
         if topic_name in self.state:
             del self.state[topic_name]
             self.save_state()
-
-    def topic_exists(self, topic_name):
-        return topic_name in self.state
-

c0a64d5582fbbb78bddd4aeadecc2e673e3cf4c2
KatherinAllin28 <68928423+KatherinAllin28@users.noreply.github.com>
2025-04-15 00:33:36 -0500

    Update global_topic.py

--- server/global_topic.py
+++ server/global_topic.py
@@ -1,9 +1,11 @@
 import redis
+from .state_manager import StateManager
 
 class GlobalTopicRegistry:
     def __init__(self, redis_host='localhost', redis_port=6379):
         """Initialize the global topic registry."""
         self.redis = redis.StrictRedis(host=redis_host, port=redis_port, decode_responses=True)
+        self.state_manager = StateManager()
 
     def create_topic(self, topic_name, num_partitions=3):
         """Create a new topic with partitions."""
@@ -11,11 +13,13 @@ class GlobalTopicRegistry:
             self.redis.sadd("topics", topic_name)
             for partition in range(num_partitions):
                 partition_key = f"{topic_name}:partition{partition}"
-             #   self.redis.lpush(partition_key, "")  # Create an empty queue for each partition
+                # Se crean llaves en Redis, aunque no se inicialicen
+                self.redis.delete(partition_key)  # Limpia si ya existe
+            self.state_manager.add_topic(topic_name, num_partitions)
             print(f"Topic '{topic_name}' created with {num_partitions} partitions.")
         else:
             print(f"Topic '{topic_name}' already exists.")
-    
+
     def list_topics(self):
         """List all topics."""
         topics = self.redis.smembers("topics")
@@ -27,6 +31,7 @@ class GlobalTopicRegistry:
             self.redis.srem("topics", topic_name)
             for key in self.redis.keys(f"{topic_name}:partition*"):
                 self.redis.delete(key)
+            self.state_manager.delete_topic(topic_name)
             print(f"Topic '{topic_name}' and its partitions deleted.")
         else:
             print(f"Topic '{topic_name}' does not exist.")
@@ -35,7 +40,6 @@ class GlobalTopicRegistry:
         """Enqueue a message to a partition of the topic."""
         partitions = self.redis.keys(f"{topic_name}:partition*")
         if partitions:
-            # Use round-robin to select a partition
             partition = partitions[hash(message) % len(partitions)]
             self.redis.rpush(partition, message)
             print(f"Message enqueued to {partition}: {message}")
